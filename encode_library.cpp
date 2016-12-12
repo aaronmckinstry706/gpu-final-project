3d64bed6d9b1cf5427f2142b1f55c470326f5e42
aaron <aaronmckinstry706@gmail.com>
2016-12-12 15:39:40 -0500

    broken; intermediate step

--- encode_library.cpp
+++ encode_library.cpp
@@ -1,7 +1,25 @@
 #include <cstdio>
 #include <vector>
+#include <map>
+#include <algorithm>
 
 #include "encode_library.h"
+
+struct word_with_code_length {
+	word_t word;
+	size_t codeLength;
+	
+	word_with_code_length(word_t w, size_t c)
+		: word(w), codeLength(c) {}
+};
+
+bool operator< (const word_with_code_length& wwcl1, const word_with_code_length& wwcl2) {
+	return wwcl1.codeLength < wwcl2.codeLength
+		|| (
+			wwcl1.codeLength == wwcl2.codeLength
+			&& wwcl1.word < wwcl2.word
+		);
+}
 
 // ---------------------------- MAIN FUNCTIONS ----------------------------------
 
@@ -29,13 +47,28 @@ std::map<word_t, size_t> getWordCodeLengths(const std::map<word_t, frequency_t>&
 	return std::map<word_t, size_t>();
 }
 
+std::map<word_t, code_t> getWordCodeTable(const std::map<word_t, size_t>& wordCodeLengths) {
+	std::vector<word_with_code_length> wordsWithCodeLengths(wordCodeLengths.size());
+	for (wordCodeLengths::const_iterator i = wordCodeLengths.begin(); i != wordCodeLenghts.end(); i++)
+		wordsWithCodeLengths.push_back(word_with_code_length(i->first, i->second));
+	std::sort(wordsWithCodeLengths.begin(), wordsWithCodeLengths.end());
+	
+	std::map<word_t, code_t> wordCodes;
+	for (size_t i = 0; i < wordsWithCodeLengths.size(); ++i) {
+		code_t code = 0;
+		for (size_t depth = 0; depth < wordsWithCodeLengths[i].codeLength; ++depth) {
+			
+		}
+	}
+}
+
 // ---------------------------- DEBUG FUNCTIONS ---------------------------------
 
 void printCharacterFrequencies(const std::map<word_t, frequency_t>& frequencies) {
 	printf("size is %d\n", frequencies.size());
 	for (std::map<word_t,frequency_t>::const_iterator i = frequencies.begin(); i != frequencies.end(); i++)
-		if ((*i).second > 0)
-			printf("%d: %lld\n", (*i).first, (*i).second);
+		if (i->second > 0)
+			printf("%d: %lld\n", i->first, i->second);
 	printf("\n");
 }
 
--- encode_library.h
+++ encode_library.h
@@ -14,7 +14,8 @@ size_t alphabetSize();
 
 std::map<word_t, frequency_t> getCharacterFrequencies(FILE *filePointer);
 std::map<word_t, size_t> getWordCodeLengths(const std::map<word_t, frequency_t>& frequencies);
-std::map<word_t, code_t> getWordCodeTable(const std::map<word_t, size_t>& wordLengths);
+std::map<word_t, code_t> getWordCodeTable(const std::map<word_t, size_t>& wordCodeLengths);
+
 
 //debug functions
 void printCharacterFrequencies(const std::map<word_t, frequency_t>& frequencies);
